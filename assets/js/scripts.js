ae375c477d2b32d15cd1b0537c54264d2faef25d
kajihasib <hasib.me1995@gmail.com>
2021-03-18 11:13:28 +0600

    animation add

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -63,6 +63,17 @@
 			stickyNav();
 		});
 	});
+	$('[data-countdown]').each(function () {
+		var $this = $(this),
+			finalDate = $(this).data('countdown');
+		$this.countdown(finalDate, function (event) {
+			$this.html(
+				event.strftime(
+					'<li class="countdown_item days"><h4>%-D</h4><span>Days</span></li><li class="countdown_item hours"><h4>%-H</h4><span>Hours</span></li><li class="countdown_item minutes"><h4>%M</h4><span>Min</span></li><li class="countdown_item second"><h4>%S</h4><span>Sec</span></li>'
+				)
+			);
+		});
+	});
 	// material
 	$('.tabs').tabs();
 	$('.collapsible').collapsible();
@@ -71,3 +82,26 @@
 	$('.material-select').formSelect();
 	$('.tooltipped').tooltip();
 })(jQuery);
+
+var textWrapper = document.querySelector('.textgit ');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime
+	.timeline({ loop: true })
+	.add({
+		targets: '.textgit  .letter',
+		translateX: [40, 0],
+		translateZ: 0,
+		opacity: [0, 1],
+		easing: 'easeOutExpo',
+		duration: 1200,
+		delay: (el, i) => 500 + 30 * i,
+	})
+	.add({
+		targets: '.textgit  .letter',
+		translateX: [0, -30],
+		opacity: [1, 0],
+		easing: 'easeInExpo',
+		duration: 1100,
+		delay: (el, i) => 100 + 30 * i,
+	});
